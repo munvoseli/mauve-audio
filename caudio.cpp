36c3cb3c33f5c7b44b7d2cc34fd4ca9edf7663c9
munvoseli <muslinmelody@gmail.com>
2021-02-04 19:54:08 -0500

    Pitches work now

--- caudio.cpp
+++ caudio.cpp
@@ -3,22 +3,21 @@
 #include <vector>
 #include <iterator>
 #include <regex>
-
+#include <cmath>
+#include <cstdint>
 // ffplay -f s16le -ar 44100 -ac 1 p.raw
 // ffplay -formats
+// ffplay -f f32le -ar 44100 -ac 1 p.raw
 
 void handleNewStringWait (std::string *lastToken, std::string *token)
 {
   
 }
 
-float getSongLength (std::string filename)
+std::string getContent (std::ifstream &infile)
 {
-  std::ifstream infile;
-  infile.open(filename + std::string(".ma"));
   std::string content( (std::istreambuf_iterator<char>(infile) ),
 		       (std::istreambuf_iterator<char>(      ) ) );
-
   // replace spaces with newlines
   std::string newline = "\n";
   std::string space = " ";
@@ -39,15 +38,20 @@ float getSongLength (std::string filename)
 	  pos++;
 	}
     }
+  return content;
+}
 
+float getSongLength (std::string content)
+{
   // search for waits and add
   // https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
-  pos = 0;
+  size_t pos = 0;
   size_t posL = 0;
   size_t posM = 0;
   int len = 0; // number of samples
   std::string lastToken;
   std::string token;
+  std::string newline = "\n";
   while ((pos = content.find(newline, posL + 1)) != std::string::npos)
     {
       pos++;
@@ -55,7 +59,6 @@ float getSongLength (std::string filename)
       token = content.substr(posL, pos - posL - 1);
       if (lastToken == "w")
 	len += 44100 * std::stoi (token);
-      //std::cout << token << std::endl;
       posM = posL;
       posL = pos;
     }
@@ -68,44 +71,75 @@ int main (int argc, char **argv)
   std::ifstream infile;
   std::ofstream outfile;
   std::string line;
-  //std::cout << "hello" << std::endl;
-  getSongLength(argv[1]);
+  std::string content;
   infile.open(argv[1] + std::string(".ma"));
-  outfile.open(argv[1] + std::string(".raw"), std::ios::out | std::ios::binary);
-  //int data [44100] = { };
+  outfile.open(argv[1] + std::string(".raw"), std::ios::out | std::ios::binary | std::ios::trunc);
+  if (!infile)
+    std::cout << "No input file :(\n";
+  if (!outfile)
+    std::cout << "Failed output file :(\n";
+  content = getContent (infile);
+  std::string newline = "\n";
   int a = 400;
-  int ar = 44100;
+  int rate = 44100;
   int freqs [16];
   int louds [16];
   int notes = 0;
-  int len = getSongLength(argv[1]);
-  int *data = new int[len];
-  if (!infile)
-      std::cout << ":(\n";
-  else
+  int len = getSongLength(content) + 1;
+  float *data = new float[len];
+  std::cout << sizeof(float) << std::endl;
+  // while (getline (infile, line) )
+  // 	{
+  // 	  if (std::string(line).substr(0, 4) == " ")
+  // 	    {
+  // 	      std::string wtimestr = std::string(line).substr (4);
+  // 	    }
+  // 	}
+  int pitch = 0;
+  int datai = 1;
+  float freq = 440;
+  size_t pos = 0;
+  size_t posL = 0;
+  size_t posM = 0;
+  std::string lastToken;
+  std::string token;
+  while ((pos = content.find(newline, posL + 1)) != std::string::npos)
     {
-      // while (getline (infile, line) )
-      // 	{
-      // 	  //std::cout << line << "\n";
-      // 	  outfile << line << "\n";
-      // 	}
-      while (getline (infile, line) )
+      pos++;
+      lastToken = content.substr(posM, posL - posM - 1);
+      token = content.substr(posL, pos - posL - 1);
+      if (lastToken == "p")
 	{
-	  if (std::string(line).substr(0, 4) == " ")
+	  int rem = std::stoi (token, 0, 12);
+	  pitch = std::round( (float) (pitch - rem) / 12.0) * 12 + rem;
+	  std::cout << pitch << std::endl;
+	  freq = 440.0 * std::pow (2.0, ((float) pitch) / 12.0);
+	}
+      else if (lastToken == "w")
+	{
+	  int goal = datai + std::stoi (token) * (float) rate;
+	  while (datai < goal)
 	    {
-	      std::string wtimestr = std::string(line).substr (4);
+	      data[datai] = data[datai - 1] + freq / (float) rate;
+	      if (data[datai] > .5)
+		data[datai] -= 1.0;
+	      data[datai];
+	      datai++;
 	    }
 	}
-      for (int i = 0; i < len; i++)
-	{
-	  data[i] = ((i % 100) * a / 100);
-	  outfile << data[i];
-	}
-      outfile.close();
-      infile.close();
+      posM = posL;
+      posL = pos;
     }
-  /*for (int i = 1; i < argc; i++)
-    std::cout << argv[i] << "\n";*/
+  for (int i = 0; i < len; i++)
+    {
+      //outfile << data[i];
+      outfile.write ( reinterpret_cast<char*>(&data[i]), sizeof(data[i]));
+    }
+  outfile.close();
+  infile.close();
+  std::cout << "Data:" << std::endl;
+  for (int i = 0; i < 10; i++)
+    std::cout << data[i] << std::endl;
   delete [] data;
   return 0;
 }
